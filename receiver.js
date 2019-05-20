e369c4f34df2ce688980f15b1621986b3c5e4c41
khrtz <garula.oze498@gmail.com>
2019-05-20 14:45:51 +0900

    Updates

--- receiver.js
+++ receiver.js
@@ -29,6 +29,8 @@ playerManager.setMessageInterceptor(
     cast.framework.messages.MessageType.LOAD,
     request => {
       castDebugLogger.info('MyAPP.LOG', 'Intercepting LOAD request');
+      
+      request.media.streamType = cast.framework.messages.StreamType.LIVE;
 
       if (request.media && request.media.entity) {
         request.media.contentId = request.media.entity;
