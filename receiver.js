ffb93a78b19b910c07b11a6bea6d77f93824ffce
khrtz <garula.oze498@gmail.com>
2019-05-20 17:58:36 +0900

    Updates

--- receiver.js
+++ receiver.js
@@ -138,7 +138,7 @@ playerDataBinder.addEventListener(
 
     // Media browse
     touchControls.setBrowseContent(browseContent);
-    castDebugLogger.info('DEV.LOG', 'browseContent', new cast.framework.ui.BrowseContent());
+    castDebugLogger.info('DEV.LOG', 'browseContent', new cast.framework.ui.fff());
   });
 
 context.start({ touchScreenOptimizedApp: true });
