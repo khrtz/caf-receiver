28e1b9112f9ccdea82c098adc02e9bba3c4e7dc4
khrtz <garula.oze498@gmail.com>
2019-05-22 12:06:43 +0900

    Updates

--- receiver.js
+++ receiver.js
@@ -63,7 +63,7 @@ playerManager.setMessageInterceptor(
 
 	            // Add metadata
 	            var metadata = new cast.framework.messages.MovieMediaMetadata();
-	            metadata.metadataType = cast.framework.messages.MetadataType.MOVIE;
+	            metadata.metadataType = cast.framework.messages.MetadataType.TV_SHOW;
 	            metadata.title = item.title;
 	            metadata.subtitle = item.author;
 
